89bd8a2c5795a071a7a784172c4cc6e44b0886fb
sixTimes9Mod7 <alexy.marcell@hotmail.com>
2016-02-14 11:24:39 +0100

    Added some physics.

--- MakeKexWeb/main.js
+++ MakeKexWeb/main.js
@@ -11,12 +11,12 @@ ctx.moveTo(0, 0);
 ctx.lineTo(100, 100);
 ctx.stroke();
 
-function avg(arr) {
-  var s = 0, c = 0; for (var i of arr) { s += i; ++c; } return s/c; }
 
 var pont = function (x,y) {
   this.x = x;
   this.y = y; }
+pont.prototype.neg = function () {
+  this.x = -this.x; this.y = -this.y; return this; }
 pont.prototype.add = function (rhs) {
   this.x += rhs.x; this.y += rhs.y; return this; }
 pont.prototype.sub = function (rhs) {
@@ -27,12 +27,19 @@ pont.prototype.mul = function (rhs) { t
   this.x *= rhs; this.y *= rhs; return this; }
 pont.prototype.div = function (rhs) {
   this.x /= rhs; this.y /= rhs; return this; }
+pont.prototype.sqrl = function () { return x*x+y*y; }
 pont.prototype.length = function() {
   return Math.sqrt(x*x + y*y); }
 pont.prototype.norm = function () {
   return this.div(this.length); }
 pont.prototype.cp = function() {
   return new pont(this.x, this.y); }
+const pont.up = new pont( 1, 0);
+const pont.dn = new pont(-1, 0);
+const pont.ri = new pont( 0, 1);
+const pont.le = new pont( 0,-1);
+const pont.nu = new pont( 0, 0);
+function negP(p) { return new pont(-p.x, -p.y); }
 function addP(p1, p2) { return new pont(p1.x+p2.x, p1.y+p2.y); }
 function subP(p1, p2) { return new pont(p1.x-p2.x, p1.y-p2.y); }
 function mulP(p, rhs) { return new pont(p.x*rhs, p.y*rhs); }
@@ -44,24 +51,30 @@ function rotP(p, o, np) {
   var rp = o.to(p);
   rp = new pont(rp.x*np.x - rp.y*np.y, rp.y*np.x + rp.x*np.y);
   return rp.add(o); }
+function inLn(p1, d1, p2, d2) {
+  return p1 + mulP(d1, crsP(subP(p2, p1), d2) / crsP(d1, d2)); }
+function inSeg(pA, pB, pP) {
+  return dotP(pA.to(pB)) / dotP(pA.to(pP)) > 1 ? pP : undefined; }
 function moveToP(p) { ctx.moveTo(p.x, p.y); }
 function lineToP(p) { ctx.lineTo(p.x, p.y); }
+
 
 var tria = function(p1, p2, p3) {
   this.p1 = p1;
   this.p2 = p2;
   this.p3 = p3; }
-tria.prototype.ponts = function* () {
-  yield this.p1; yield this.p2; yield this.p3; }
+this.prototype.a = function () { return p1.to(p2); }
+this.prototype.b = function () { return p2.to(p3); }
+this.prototype.c = function () { return p3.to(p1); }
 tria.prototype.edges = function* () {
-  yield this.p1.to(this.p2);
-  yield this.p2.to(this.p3);
-  yield this.p3.to(this.p1); }
+  yield [this.p1, this.p1.to(this.p2)];
+  yield [this.p2, this.p2.to(this.p3)];
+  yield [this.p3, this.p3.to(this.p1)]; }
 tria.prototype.area = function () {
-  return Math.abs(this.p2.sub(this.p1).crs(p3.sub(this.p1)))/2; }
-tria.prototype.median = function () {
+  return Math.abs(crsP(this.p1.to(this.p2), this.p1.to(this.p3))/2); }
+tria.prototype.cen = function () {
   return addP(this.p1, addP(this.p2, this.p3)).div(3); }
-tria.prototype.isIn = function (p) {
+tria.prototype.isPIn = function (p) {
   var z1 = crsP(this.p1.to(p2), this.p1.to(p));
   var z2 = crsP(this.p2.to(p3), this.p2.to(p));
   var z3 = crsP(this.p3.to(p1), this.p3.to(p));
@@ -72,7 +85,7 @@ tria.prototype.draw = function () {
   moveToP(this.p3);
   lineToP(this.p1);
   lineToP(this.p2);
-  lineToP(this.p3);
+  lineToP(tis.p3);
   ctx.stroke(); }
 tria.prototype.mov = function (vt) {
   this.p1.add(vt); this.p2.add(vt); this.p3.add(vt); }
@@ -80,12 +93,37 @@ tria.prototype.rot = function (o, np) {
   this.p1 = rotP(this.p1, o, np);
   this.p2 = rotP(this.p2, o, np);
   this.p3 = rotP(this.p3, o, np); }
-function crossTr(tr1, tr2) {
-  for (var p of tr1.ponts()) {
-    if(tr2.isIn(p)) return true;  }
-  for (var p of tr2.ponts()) {
-    if(tr1.isIn(p)) return true;  }
-  return false; }
+function crsTr(tr1, tr2) {
+  var rp = [];
+  for(var e1 of tr1.edges()) {
+  for(var e2 of tr2.edges()) {
+    rp.push(inSeg(inLn(e1[0], e1[1], e2[0], e2[1])));
+  } }
+  return rp.length() ? rp : undefined; }
+
+var rig = function (tri, imas = Infinity, isped = pont.nu, iangs = 0) {
+  this.tri = tri;
+  this.mas = imas;
+  this.spe = isped;
+  this.ang = iangs; }
+rig.prototype.anm = function () {
+  return this.mas * ( this.tri.a().sqrl() +
+    this.tri.area()*this.tri.area() / this.tri.a().sqrl() +
+    dotP(this.tri.a(),this.tri.b())*(dotP(this.tri.a(), this.tri.b())-1))/36; }
+rig.prototype.push = function (inp, st = this.tri.cen()) {
+  this.spe += mulP(inp, this.mas);
+  this.ang += this.tri.cen().to(st).sqrl() &&
+    crsP(inp, this.tri.cen().to(st)) * this.anm(); }
+function coll (rig1, rig2) {
+  var cp = crsTr(rig1.tri, rig2.tri);
+  if(!cp) return;
+  var nrm = rig1.tri.cen().to(rig2.tri.cen()).norm();
+  var rel = subP(rig2.spe(), rig1.spe());
+  nrm.mul(dotP(nrm, rel)/cp.length());
+  for(var p of cp) {
+    rig1.push(nrm, p);
+    rig2.push(negP(nrm), p);
+  } }
 
 var tri = new tria(new pont(150,200), new pont(215,50), new pont(100,110))
 
@@ -94,8 +132,6 @@ function loopMain (dt) {
   ctx.clearRect(0, 0, canv.width, canv.height);
   ctx.fillText(1/dt, 0, canv.height-30);
   ctx.beginPath();
-  ctx.moveTo(-1e48, 50);
-  ctx.lineTo(1e48, 50)
   ctx.stroke();
   tri.draw();
   tri.rot(new pont(200,200), phiP(dt*Math.PI))
@@ -107,5 +143,3 @@ var loop = setInterval(function () {
     loopMain((-last.getTime() + (last=new Date()).getTime())/1000);
     step++; }
 }(), 5);
-
-//*/
