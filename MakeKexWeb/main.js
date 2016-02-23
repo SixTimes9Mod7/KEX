667cba2430c50d412642608e597b2c05d1741e7a
sixTimes9Mod7 <alexy.marcell@hotmail.com>
2016-02-23 22:54:55 +0100

    Working basic triangle physic.

--- MakeKexWeb/main.js
+++ MakeKexWeb/main.js
@@ -14,31 +14,33 @@ ctx.stroke();
 
 var pont = function (x,y) {
   this.x = x;
-  this.y = y; }
+  this.y = y;
+  return this;}
 pont.prototype.neg = function () {
   this.x = -this.x; this.y = -this.y; return this; }
 pont.prototype.add = function (rhs) {
   this.x += rhs.x; this.y += rhs.y; return this; }
 pont.prototype.sub = function (rhs) {
   this.x -= rhs.x; this.y -= rhs.y; return this; }
-pont.prototype.to = function(to) {
-  return new pont(to.x - this.x, to.y - this.y); }
-pont.prototype.mul = function (rhs) { t
+pont.prototype.to = function(tt) {
+  return new pont(tt.x - this.x, tt.y - this.y); }
+pont.prototype.mul = function (rhs) {
   this.x *= rhs; this.y *= rhs; return this; }
 pont.prototype.div = function (rhs) {
   this.x /= rhs; this.y /= rhs; return this; }
-pont.prototype.sqrl = function () { return x*x+y*y; }
+pont.prototype.sqrl = function () {
+  return this.x*this.x+this.y*this.y; }
 pont.prototype.length = function() {
-  return Math.sqrt(x*x + y*y); }
+  return Math.sqrt(this.x*this.x + this.y*this.y); }
 pont.prototype.norm = function () {
-  return this.div(this.length); }
+  return this.div(this.length()); }
 pont.prototype.cp = function() {
   return new pont(this.x, this.y); }
-const pont.up = new pont( 1, 0);
-const pont.dn = new pont(-1, 0);
-const pont.ri = new pont( 0, 1);
-const pont.le = new pont( 0,-1);
-const pont.nu = new pont( 0, 0);
+pont.up = new pont( 1, 0);
+pont.dn = new pont(-1, 0);
+pont.ri = new pont( 0, 1);
+pont.le = new pont( 0,-1);
+pont.nu = new pont( 0, 0);
 function negP(p) { return new pont(-p.x, -p.y); }
 function addP(p1, p2) { return new pont(p1.x+p2.x, p1.y+p2.y); }
 function subP(p1, p2) { return new pont(p1.x-p2.x, p1.y-p2.y); }
@@ -52,9 +54,12 @@ function rotP(p, o, np) {
   rp = new pont(rp.x*np.x - rp.y*np.y, rp.y*np.x + rp.x*np.y);
   return rp.add(o); }
 function inLn(p1, d1, p2, d2) {
-  return p1 + mulP(d1, crsP(subP(p2, p1), d2) / crsP(d1, d2)); }
-function inSeg(pA, pB, pP) {
-  return dotP(pA.to(pB)) / dotP(pA.to(pP)) > 1 ? pP : undefined; }
+  return addP(p1, mulP(d1, crsP(subP(p2, p1), d2) / crsP(d1, d2))); }
+function inCirc(pP, pA, pB) {
+  return pA.to(pB).sqrl() / pA.to(pP).sqrl() > 1 ? pP : undefined; }
+function inSeg(p1, d1, p2, d2) {
+  var c = crsP(subP(p2, p1), d2) / crsP(d1, d2);
+  if(1/c > 1) return addP(p1, mulP(d1, c)); }
 function moveToP(p) { ctx.moveTo(p.x, p.y); }
 function lineToP(p) { ctx.lineTo(p.x, p.y); }
 
@@ -62,10 +67,11 @@ function lineToP(p) { ctx.lineTo(p.x, p.y); }
 var tria = function(p1, p2, p3) {
   this.p1 = p1;
   this.p2 = p2;
-  this.p3 = p3; }
-this.prototype.a = function () { return p1.to(p2); }
-this.prototype.b = function () { return p2.to(p3); }
-this.prototype.c = function () { return p3.to(p1); }
+  this.p3 = p3;
+  return this; }
+tria.prototype.a = function () { return this.p1.to(this.p2); }
+tria.prototype.b = function () { return this.p2.to(this.p3); }
+tria.prototype.c = function () { return this.p3.to(this.p1); }
 tria.prototype.edges = function* () {
   yield [this.p1, this.p1.to(this.p2)];
   yield [this.p2, this.p2.to(this.p3)];
@@ -85,11 +91,11 @@ tria.prototype.draw = function () {
   moveToP(this.p3);
   lineToP(this.p1);
   lineToP(this.p2);
-  lineToP(tis.p3);
+  lineToP(this.p3);
   ctx.stroke(); }
 tria.prototype.mov = function (vt) {
   this.p1.add(vt); this.p2.add(vt); this.p3.add(vt); }
-tria.prototype.rot = function (o, np) {
+tria.prototype.rot = function (np, o = this.cen()) {
   this.p1 = rotP(this.p1, o, np);
   this.p2 = rotP(this.p2, o, np);
   this.p3 = rotP(this.p3, o, np); }
@@ -97,9 +103,10 @@ function crsTr(tr1, tr2) {
   var rp = [];
   for(var e1 of tr1.edges()) {
   for(var e2 of tr2.edges()) {
-    rp.push(inSeg(inLn(e1[0], e1[1], e2[0], e2[1])));
+    mp = inSeg(e1[0], e1[1], e2[0], e2[1]);
+    if(mp) rp.push(mp);
   } }
-  return rp.length() ? rp : undefined; }
+  return rp.length ? rp : undefined; }
 
 var rig = function (tri, imas = Infinity, isped = pont.nu, iangs = 0) {
   this.tri = tri;
@@ -107,34 +114,42 @@ var rig = function (tri, imas = Infinity, isped = pont.nu, iangs = 0) {
   this.spe = isped;
   this.ang = iangs; }
 rig.prototype.anm = function () {
-  return this.mas * ( this.tri.a().sqrl() +
+  return this.mas * (this.tri.a().sqrl() +
     this.tri.area()*this.tri.area() / this.tri.a().sqrl() +
-    dotP(this.tri.a(),this.tri.b())*(dotP(this.tri.a(), this.tri.b())-1))/36; }
+    dotP(this.tri.a(),this.tri.b())*(dotP(this.tri.a(), this.tri.b())-1)
+    )/36; }
 rig.prototype.push = function (inp, st = this.tri.cen()) {
-  this.spe += mulP(inp, this.mas);
-  this.ang += this.tri.cen().to(st).sqrl() &&
-    crsP(inp, this.tri.cen().to(st)) * this.anm(); }
+  this.spe.add(divP(inp, this.mas));
+  this.ang -= crsP(inp, this.tri.cen().to(st)) / this.anm(); }
+rig.prototype.step = function (dt) {
+  this.tri.mov(mulP(this.spe,dt));
+  this.tri.rot(phiP(this.ang*dt)); }
 function coll (rig1, rig2) {
   var cp = crsTr(rig1.tri, rig2.tri);
   if(!cp) return;
   var nrm = rig1.tri.cen().to(rig2.tri.cen()).norm();
-  var rel = subP(rig2.spe(), rig1.spe());
-  nrm.mul(dotP(nrm, rel)/cp.length());
+  var rel = subP(rig2.spe, rig1.spe);
+  if(dotP(nrm, rel) < 0) {
+    nrm.mul(dotP(nrm, rel)/cp.length*(rig1.mas + rig2.mas)); }
   for(var p of cp) {
     rig1.push(nrm, p);
     rig2.push(negP(nrm), p);
   } }
 
-var tri = new tria(new pont(150,200), new pont(215,50), new pont(100,110))
+var rigby = new rig(new tria(new pont(150,200), new pont(215,50),
+  new pont(100,110)), 10, new pont(0, 0), 1/3*Math.PI);
+var r2 = new rig(new tria(new pont(350, 200), new pont(350, 30),
+  new pont(300, 25)), 20, new pont(-20, 0), 0);
 
 var step = 0;
 function loopMain (dt) {
   ctx.clearRect(0, 0, canv.width, canv.height);
   ctx.fillText(1/dt, 0, canv.height-30);
-  ctx.beginPath();
-  ctx.stroke();
-  tri.draw();
-  tri.rot(new pont(200,200), phiP(dt*Math.PI))
+  rigby.step(dt);
+  r2.step(dt);
+  rigby.tri.draw();
+  r2.tri.draw();
+  coll(rigby, r2);
 }
 
 var loop = setInterval(function () {
@@ -142,4 +157,4 @@ var loop = setInterval(function () {
   return function () {
     loopMain((-last.getTime() + (last=new Date()).getTime())/1000);
     step++; }
-}(), 5);
+}(), 10);
